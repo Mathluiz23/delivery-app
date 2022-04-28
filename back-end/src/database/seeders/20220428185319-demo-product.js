d768414b72e29550514479474bfff41188e59064
Vanim77 <vanimouzad123@gmail.com>
2022-04-28 18:24:36 -0300

    :sparkles: feat: implements product seeds

---
+++ back-end/src/database/seeders/20220428185319-demo-product.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('products', [{
+      id: 1,
+      name: 'Skol Lata 250ml',
+      price: 2.20,
+      url_image: 'http://localhost:3001/images/skol_lata_350ml.jpg',
+    },
+    {
+      id: 2,
+      name: 'Heineken 600ml',
+      price: 7.50,
+      url_image: 'http://localhost:3001/images/heineken_600ml.jpg',
+    },
+    {
+      id: 3,
+      name: 'Antarctica Pilsen 300ml',
+      price: 2.49,
+      url_image: 'http://localhost:3001/images/antarctica_pilsen_300ml.jpg',
+    }
+  ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('products', null, {});
+  }
+};
