5e1fd9654c460cb40cb01c1b35c2e5f0e57e15db
Vanim77 <vanimouzad123@gmail.com>
2022-05-02 17:21:39 -0300

    :recycle: refactor: regex on handleLogin

--- front-end/src/components/login.js
+++ front-end/src/components/login.js
@@ -4,13 +4,17 @@ function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = (email, password) => {
-    const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+  const handleLogin = (userEmail, userPassword) => {
+    const minPasswordLength = 6;
+    // Regex retirado do site
+    // https://pt.stackoverflow.com/questions/1386/expressão-regular-para-validação-de-e-mail
 
-    if (regex.test(email) && password.length >= 6) return false;
+    const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+
+    if (regex.test(userEmail) && userPassword.length >= minPasswordLength) return false;
 
     return true;
-  }
+  };
 
   return (
     <>
@@ -42,7 +46,7 @@ function Login() {
         <button
           data-testid="common_login__button-login"
           type="submit"
-          disabled={ () => handleLogin(email, password) }
+          disabled={ handleLogin(email, password) }
         >
           Login
         </button>
