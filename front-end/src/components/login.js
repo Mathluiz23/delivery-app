1150ac3a45ce61e228af8a39ae46fdc434302438
juliana <juliana4braga@gmail.com>
2022-05-02 16:45:27 -0300

    :sparkles: feat: Validação de login

--- front-end/src/components/login.js
+++ front-end/src/components/login.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (email, password) => {
+    const regex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i;
+
+    if (regex.test(email) && password.length >= 6) return false;
+
+    return true;
+  }
+
   return (
     <>
       <img alt="beer" src="https://w7.pngwing.com/pngs/644/125/png-transparent-beer-festival-computer-icons-food-beer-food-text-beer-festival.png" width={ 40 } height={ 40 } />
@@ -12,7 +23,9 @@ function Login() {
             data-testid="common_login__input-email"
             id="email"
             type="text"
+            value={ email }
             placeholder="example: email@test.com"
+            onChange={ (e) => setEmail(e.target.value) }
           />
         </label>
         <label htmlFor="password">
@@ -21,12 +34,15 @@ function Login() {
             data-testid="common_login__input-password"
             id="password"
             type="password"
+            value={ password }
             placeholder="digite sua senha..."
+            onChange={ (e) => setPassword(e.target.value) }
           />
         </label>
         <button
           data-testid="common_login__button-login"
           type="submit"
+          disabled={ () => handleLogin(email, password) }
         >
           Login
         </button>
