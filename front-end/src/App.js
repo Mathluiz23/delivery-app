61b4fcaa31735dcee4eeca34c37a754118ea2109
taicout6 <taicout6@gmail.com>
2022-05-03 16:22:25 -0300

    Fix: Front-end index.js and app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginPage from './pages/loginPage';
 
-function App() {
+export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/login" element={ <LoginPage /> } />
-        <Route exact path="/" element={ <Navigate to="/login" /> } />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route exact path="/login" element={ <LoginPage /> } />
+      <Route exact path="/" element={ <Navigate to="/login" /> } />
+    </Routes>
   );
 }
-
-export default App;
