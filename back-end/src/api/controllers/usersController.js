b113beccad0529e7cdfd2f85502887e0c6a84cfd
Vanim77 <vanimouzad123@gmail.com>
2022-05-04 16:30:12 -0300

    :recycle: refactor: function getUser

--- back-end/src/api/controllers/usersController.js
+++ back-end/src/api/controllers/usersController.js
@@ -1,11 +1,13 @@
+const encryptPassword = require('../helpers/encryptPassword');
 const StatusCode = require('../helpers/StatusCode');
 const UsersService = require('../services/usersServices');
 
 const usersController = {
   getUser: async (req, res) => {
     const { email } = req.body;
+    const password = encryptPassword(req.body.password);
 
-    const response = await UsersService.findByEmail(email);
+    const response = await UsersService.getUser(email, password);
   
     if (response.message) {
       return res.status(StatusCode.NotFound).json(response);
