67095c7540c79e5229e554e1ace8236213418d4c
Vanim77 <vanimouzad123@gmail.com>
2022-05-04 19:00:56 -0300

    :bug: fix: webvitals import bug

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,15 +1,18 @@
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-const rootElement = document.getElementById("root");
-render(
+ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>,
-  rootElement
+  document.getElementById('root'),
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
