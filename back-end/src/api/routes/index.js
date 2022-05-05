001087734a3e43f363835b2638b768757b1c9729
Vanim77 <vanimouzad123@gmail.com>
2022-05-05 16:50:06 -0300

    :sparkles: feat: registerRoute

--- back-end/src/api/routes/index.js
+++ back-end/src/api/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const loginRouter = require('./loginRoute');
+const registerRouter = require('./registerRoute');
 
 const router = Router();
 
 router.use(loginRouter);
+router.use(registerRouter);
 
 module.exports = router;
