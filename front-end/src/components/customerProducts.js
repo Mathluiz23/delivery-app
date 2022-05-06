c5d9d2472aa772f1ed234cf1224a14d02e8dd91e
Vanim77 <vanimouzad123@gmail.com>
2022-05-06 15:12:00 -0300

    :ok_hand: style: fixing lint errors

--- front-end/src/components/customerProducts.js
+++ front-end/src/components/customerProducts.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
 function CustomerProducts() {
-  <div>
-    <nav>
-      <ul>
-        <li data-testid="customer_products__element-navbar-link-products">Produtos</li>
-        <li data-testid="customer_products__element-navbar-link-orders">Meus pedidos</li>
-        <li data-testid="customer_products__element-navbar-user-full-name">Nome</li>
-        <li data-testid="customer_products__element-navbar-link-logout">Sair</li>
-      </ul>
-    </nav>
-  </div>
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li
+            data-testid="customer_products__element-navbar-link-products"
+          >
+            Produtos
+          </li>
+          <li
+            data-testid="customer_products__element-navbar-link-orders"
+          >
+            Meus pedidos
+          </li>
+          <li
+            data-testid="customer_products__element-navbar-user-full-name"
+          >
+            Nome
+          </li>
+          <li
+            data-testid="customer_products__element-navbar-link-logout"
+          >
+            Sair
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 
 export default CustomerProducts;
