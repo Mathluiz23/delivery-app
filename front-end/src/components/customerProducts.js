7430150b00841d76b2f238da360ed56d7874f69b
juliana <juliana4braga@gmail.com>
2022-05-06 16:52:48 -0300

    :bug: fix: accept changes

--- front-end/src/components/customerProducts.js
+++ front-end/src/components/customerProducts.js
@@ -1,18 +1,5 @@
 import React from 'react';
 
-<<<<<<< HEAD
-function CustomerProducts () {
-  <div>
-    <nav>
-      <ul>
-        <li data-testid="customer_products__element-navbar-link-products">Produtos</li>
-        <li data-testid="customer_products__element-navbar-link-orders">Meus pedidos</li>
-        <li data-testid="customer_products__element-navbar-user-full-name">Nome</li>
-        <li data-testid="customer_products__element-navbar-link-logout">Sair</li>
-      </ul>
-    </nav>
-  </div>
-=======
 function CustomerProducts() {
   return (
     <div>
@@ -42,7 +29,6 @@ function CustomerProducts() {
       </nav>
     </div>
   );
->>>>>>> c5d9d2472aa772f1ed234cf1224a14d02e8dd91e
 }
 
 export default CustomerProducts;
