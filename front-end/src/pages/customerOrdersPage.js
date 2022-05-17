deb6e308731bbbd177bf9eb3f9e4940ae6e4b1cd
juliana <juliana4braga@gmail.com>
2022-05-16 21:24:46 -0300

    :sparkles: feat: CustomerOrdersPage

---
+++ front-end/src/pages/customerOrdersPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBarCustomer from '../components/customer/navBarCustomer';
+
+function CustomerOrdersPage() {
+  return(
+    <>
+      <NavBarCustomer />
+      
+    </>
+  );
+}
+
+export default CustomerOrdersPage;
