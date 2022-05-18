29f030a075ca77841592ba47358881c9ee4ff3ab
Vanim77 <vanimouzad123@gmail.com>
2022-05-17 22:31:23 -0300

    :sparkles: feat: sellerOrdersPage

---
+++ front-end/src/pages/sellerOrdersPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SellerOrders from '../components/seller/sellerOrders';
+
+function SellerOrdersPage() {
+  return (
+    <SellerOrders />
+  );
+}
+
+export default SellerOrdersPage;
