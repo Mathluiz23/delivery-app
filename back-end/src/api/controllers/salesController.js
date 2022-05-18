42ea4fb3d61ec711ed7d69f468959b5773382737
Vanim77 <vanimouzad123@gmail.com>
2022-05-17 22:09:54 -0300

    :recycle: refactor: await on async function

--- back-end/src/api/controllers/salesController.js
+++ back-end/src/api/controllers/salesController.js
@@ -4,7 +4,7 @@ const salesErrors = require('../errors/salesErrors');
 
 const salesController = {
   read: async (_req, res) => {
-    const response = salesService.read();
+    const response = await salesService.read();
 
     if (response.message) {
       return res.status(StatusCode.InternalServerError).json(salesErrors.serverError);
