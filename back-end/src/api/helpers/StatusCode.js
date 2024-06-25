e23b0dab3d1c534b33116a55fe3b21a8034d40b8
Mathluiz23 <matheus_luizs@hotmail.com>
2024-06-25 22:16:21 +1200

    status code satand by to implement after

--- back-end/src/api/helpers/StatusCode.js
+++ back-end/src/api/helpers/StatusCode.js
@@ -6,6 +6,11 @@ const StatusCode = {
   NotFound: 404,
   Conflict: 409,
   InternalServerError: 500,
+ 
+  // Forbidden: 403,
+  // MethodNotAllowed: 405,
+  // UnprocessableEntity: 422,
+  // TooManyRequests: 429,
 };
 
 module.exports = StatusCode;
